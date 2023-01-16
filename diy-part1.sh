72076f71f28feb3983d029c076e977e34bd427b3
lajidon <59024831+lajidon@users.noreply.github.com>
2023-01-17 01:22:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,3 +22,26 @@
 git clone https://github.com/kenzok78/luci-theme-argonne.git package/luci-theme-argonne
 
 ##############################################################################################
+# 实时监控
+git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
+#释放内存
+git clone https://github.com/chinaboy2345/luci-app-ramfree.git package/luci-app-ramfree
+
+#磁盘管理
+git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+
+#  关机
+git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
+
+#docker
+git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+
+#ShadowSocksR Plus+
+git clone https://github.com/fw876/helloworld.git package/helloworld
+
+#openclash
+git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+
+#文件助手
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-fileassistant package/luci-app-fileassistant
+
