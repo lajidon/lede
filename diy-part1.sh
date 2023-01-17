30e3f5027ebbc699b75016f6ebeb62c3d2bf2db8
lajidon <59024831+lajidon@users.noreply.github.com>
2023-01-17 19:54:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,25 +23,26 @@ git clone https://github.com/kenzok78/luci-theme-argonne.git package/luci-theme-
 
 ##############################################################################################
 # 实时监控
-git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
+#git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
+
 #释放内存
-git clone https://github.com/chinaboy2345/luci-app-ramfree.git package/luci-app-ramfree
+#git clone https://github.com/chinaboy2345/luci-app-ramfree.git package/luci-app-ramfree
 
 #磁盘管理
-git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+#git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
 
 #  关机
-git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
+#git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
 
 #docker
-git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+#git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
 
 #ShadowSocksR Plus+
-git clone https://github.com/fw876/helloworld.git package/helloworld
+#git clone https://github.com/fw876/helloworld.git package/helloworld
 
 #openclash
-git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+#git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 
 #文件助手
-svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-fileassistant package/luci-app-fileassistant
+#svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-fileassistant package/luci-app-fileassistant
 
